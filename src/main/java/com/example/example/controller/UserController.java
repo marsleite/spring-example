bb8ecc4ba25f56b41591a9f91e935cc5a64ecb7e
Marcelo <marcelo.leite@mercadolivre.com>
2022-03-27 19:47:33 -0300

    [feat] criação de endpoint de users

--- src/main/java/com/example/example/controller/UserController.java
+++ src/main/java/com/example/example/controller/UserController.java
@@ -1,15 +1,36 @@
 package com.example.example.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.example.dto.UserDTO;
+import com.example.example.entity.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+  private List<User> users = new ArrayList<>();
 
-  @GetMapping("/")
-  public String hello(){
-    return "Hello world";
+  @GetMapping("/lista")
+  public List<User> getUsers() {
+    return users;
+  }
+
+  @PostMapping("/")
+  public List<User> createUser(@RequestBody User user){
+    users.add(user);
+    return users;
+  }
+
+  @GetMapping("/{id}")
+  public User getUserById(@PathVariable("id") Long id) {
+    Optional<User> data = users.stream().filter(user -> user.getId() == id).findFirst();
+
+    if(data.isPresent()) {
+      return data.get();
+    }
+    return null;
   }
 }
---
+++ src/main/java/com/example/example/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.example.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+  private String name;
+  private String username;
+}
